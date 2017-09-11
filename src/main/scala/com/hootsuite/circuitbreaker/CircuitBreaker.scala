3ba2af067ee0bfb287148a2af8b705d3c50a8efa
Thomas W. Watson <drclick@mac.com>
2017-09-11 11:56:49 -0700

    added exponentialRetryCap, attemptReset state, and cleaned up tests in general

--- src/main/scala/com/hootsuite/circuitbreaker/CircuitBreaker.scala
+++ src/main/scala/com/hootsuite/circuitbreaker/CircuitBreaker.scala
@@ -10,57 +10,43 @@ import scala.util.{Failure, Success, Try}
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.{AtomicInteger, AtomicReference}
 
+import com.hootsuite.circuitbreaker.CircuitBreaker._
+
 /**
   * A configurable circuit breaker. For the time being, the only state change detection strategy implemented is based on
   * the number of consecutive failures.
   *
-  * @param name the name of the circuit breaker
-  * @param failLimit maximum number of consecutive failures before the circuit breaker is tripped (opened)
-  * @param retryDelay duration until an open/broken circuit breaker lets a call through to verify whether or not it should be reset
-  * @param isResultFailure partial function to allow users to determine return cases which should be considered as failures
+  * @param name                  the name of the circuit breaker
+  * @param failLimit             maximum number of consecutive failures before the circuit breaker is tripped (opened)
+  * @param retryDelay            duration until an open/broken circuit breaker lets a call through to verify whether or not it should be reset
+  * @param isExponentialBackoff  indicating the retry delayed should be increased exponential on consecutive failures
+  * @param exponentialRetryCap   limits the number of times the retryDelay will be increased exponentially, ignored if not exponential backoff
+  * @param isResultFailure       partial function to allow users to determine return cases which should be considered as failures
   * @param isExceptionNotFailure partial function to allow users to determine exceptions which should not be considered failures
-  * @param stateChangeListeners listeners that will be notified when the circuit breaker changes state (open <--> closed)
-  * @param invocationListeners listeners that will be notified whenever the circuit breaker handles a method/function call
+  * @param stateChangeListeners  listeners that will be notified when the circuit breaker changes state (open <--> closed)
+  * @param invocationListeners   listeners that will be notified whenever the circuit breaker handles a method/function call
   */
 class CircuitBreaker private[circuitbreaker] (
   val name: String,
   val failLimit: Int,
   val retryDelay: FiniteDuration,
   val isExponentialBackoff: Boolean = false,
-  val isResultFailure: PartialFunction[Any, Boolean] = { case _ => false },
-  val isExceptionNotFailure: PartialFunction[Throwable, Boolean] = { case _ => false },
+  val exponentialRetryCap: Option[Int],
+  val isResultFailure: PartialFunction[Any, Boolean] = {
+    case _ => false
+  },
+  val isExceptionNotFailure: PartialFunction[Throwable, Boolean] = {
+    case _ => false
+  },
   val stateChangeListeners: List[CircuitBreakerStateChangeListener] = List(),
   val invocationListeners: List[CircuitBreakerInvocationListener] = List(),
   val notificationsExecutionContext: ExecutionContext
 ) extends ReadOnlyCircuitBreakerSnapshot {
 
-  private[circuitbreaker] def this(builder: CircuitBreakerBuilder) =
-    this(
-      builder.name,
-      builder.failLimit,
-      builder.retryDelay,
-      builder.isExponentialBackoff,
-      builder.isResultFailure,
-      builder.isExceptionNotFailure,
-      builder.stateChangeListeners,
-      builder.invocationListeners,
-      builder.notificationsExecutionContext.getOrElse(
-        ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
-      )
-    )
-
-  import com.hootsuite.circuitbreaker.CircuitBreaker._
-
   // keep state
   private[this] val state = new AtomicReference[State](new FlowState(this))
 
-  // register self with circuit breaker registry
-  CircuitBreakerRegistry.register(this)
-
-  // notify of initialization
-  stateChangeListeners.foreach { listener =>
-    safely(notificationsExecutionContext)(listener.onInit, name, "notify listener of being initialized")
-  }
+  import com.hootsuite.circuitbreaker.CircuitBreaker._
 
   def apply[T](fallback: Option[Try[T]] = None)(block: => T): T =
     Try {
@@ -84,28 +70,13 @@ class CircuitBreaker private[circuitbreaker] (
         }
     }
 
-  def async[T](
-    fallback: Option[Try[T]] = None
-  )(block: => Future[T])(implicit ec: ExecutionContext): Future[T] =
-    Try {
-      state.get.preInvoke()
-    } match {
+  // register self with circuit breaker registry
+  CircuitBreakerRegistry.register(this)
 
-      //check if we should return a fallback
-      case Failure(_: CircuitBreakerBrokenException) if fallback.isDefined =>
-        logger.debug(
-          s"Circuit breaker \'$name\' in broken/open state, returning fallback value: ${fallback.get}"
-        )
-        fallback.get.toFuture
-      case Failure(e) =>
-        Future.failed(e)
-      case Success(_) =>
-        block.map { ret =>
-          handleCallReturnedValue(ret)
-        }.recover {
-          handleFailedCall
-        }
-    }
+  // notify of initialization
+  stateChangeListeners.foreach { listener =>
+    safely(notificationsExecutionContext)(listener.onInit, name, "notify listener of being initialized")
+  }
 
   private def handleCallReturnedValue[T](ret: T): T = {
 
@@ -155,6 +126,29 @@ class CircuitBreaker private[circuitbreaker] (
         false
     }.get
 
+  def async[T](
+    fallback: Option[Try[T]] = None
+  )(block: => Future[T])(implicit ec: ExecutionContext): Future[T] =
+    Try {
+      state.get.preInvoke()
+    } match {
+
+      //check if we should return a fallback
+      case Failure(_: CircuitBreakerBrokenException) if fallback.isDefined =>
+        logger.debug(
+          s"Circuit breaker \'$name\' in broken/open state, returning fallback value: ${fallback.get}"
+        )
+        fallback.get.toFuture
+      case Failure(e) =>
+        Future.failed(e)
+      case Success(_) =>
+        block.map { ret =>
+          handleCallReturnedValue(ret)
+        }.recover {
+          handleFailedCall
+        }
+    }
+
   /**
     * Switch to open/broken state.
     */
@@ -179,12 +173,60 @@ class CircuitBreaker private[circuitbreaker] (
 
   /**
     * Try to restart the open/broken state.
+    *
     * @param currentState the expected current state
     * @return true when the state was changed, false when the given state was not the current state
     */
-  def attemptResetBrokenState(currentState: BrokenState, retryCount: Int): Boolean = {
+  def attemptResetBrokenState(currentState: State, retryCount: Int): Boolean = {
     logger.debug(s"Circuit breaker \'$name\', attempting to reset open/broken state")
-    state.compareAndSet(currentState, new BrokenState(this, retryCount))
+    val result =
+      state.compareAndSet(currentState, new AttemptResetState(this, retryCount))
+
+    if (result) {
+      stateChangeListeners.foreach { listener =>
+        safely(notificationsExecutionContext)(
+          listener.onAttemptReset,
+          name,
+          "notify listener of reset attempt"
+        )
+      }
+    }
+    result
+  }
+
+  /**
+    * calculate the new retry
+    *
+    * @param retryCount what retry is it
+    * @return the number of ms to wait before retrying
+    */
+  def calcRetryDelay(retryCount: Int): Long = {
+
+    //calc jitter up to 1/10 the current retryDelay
+    //for exponential backoff
+    val jitter: Long = if (this.isExponentialBackoff) {
+      (scala.util.Random.nextFloat() * this.retryDelay.toMillis / 10).toLong
+    } else {
+      0
+    }
+
+    val retryCap = this.exponentialRetryCap match {
+      case Some(x) => x
+      case None => Int.MaxValue
+    }
+
+    val exponent: Int = if (this.isExponentialBackoff) {
+      Math.min(retryCount, retryCap)
+    } else 0
+
+    val result = (this.retryDelay.toMillis + jitter) * Math.pow(2, exponent).toLong
+
+    logger.debug(
+      s"CB retry delay details: jitter $jitter, " +
+        s"retryCap $retryCap, exponent $exponent delay $result"
+    )
+
+    result
   }
 
   /**
@@ -198,18 +240,36 @@ class CircuitBreaker private[circuitbreaker] (
   /**
     * @inheritdoc
     */
-  override def isBroken: Boolean = state.get() match {
-    case _: BrokenState => true
-    case _ => false
-  }
+  override def isBroken: Boolean =
+    state.get() match {
+      case _: BrokenState => true
+      case _: AttemptResetState => true
+      case _ => false
+    }
 
   /**
     * @inheritdoc
     */
   override def isWaiting: Boolean = state.get() match {
-    case s: BrokenState if System.currentTimeMillis() > s.retryAt => true
+    case _: AttemptResetState => true
     case _ => false
   }
+
+  private[circuitbreaker] def this(builder: CircuitBreakerBuilder) =
+    this(
+      builder.name,
+      builder.failLimit,
+      builder.retryDelay,
+      builder.isExponentialBackoff,
+      builder.exponentialRetryCap,
+      builder.isResultFailure,
+      builder.isExceptionNotFailure,
+      builder.stateChangeListeners,
+      builder.invocationListeners,
+      builder.notificationsExecutionContext.getOrElse(
+        ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
+      )
+    )
 }
 
 /**
@@ -235,10 +295,12 @@ private object CircuitBreaker {
     }
   }
 
+  private val logger = LoggerFactory.getLogger(getClass)
+
   private def safely(
     ec: ExecutionContext
   )(op: => (String) => Any, str: String, opName: String = "<no operation name specified>"): Unit = {
-    implicit val implicitEc = ec
+    implicit val implicitEc: ExecutionContext = ec
     Future(op(str)).recover {
       case NonFatal(e) =>
         logger.warn(
@@ -247,8 +309,6 @@ private object CircuitBreaker {
         )
     }
   }
-
-  private val logger = LoggerFactory.getLogger(getClass)
 
   sealed trait State {
 
@@ -300,11 +360,16 @@ private object CircuitBreaker {
   /**
     * CircuitBreaker is opened/broken. Invocations fail immediately.
     */
-  class BrokenState(cb: CircuitBreaker, retryCount: Int = 0) extends State {
-    val retryDelay: Long = cb.retryDelay.toMillis * Math
-      .pow(2, if (cb.isExponentialBackoff) retryCount else 0)
-      .toLong
-    val retryAt: Long = System.currentTimeMillis() + retryDelay
+  class BrokenState(cb: CircuitBreaker) extends State {
+    val retryDelay: Long = cb.calcRetryDelay(0)
+
+    //Automatically transition this state at the retry time
+    implicit val ec: ExecutionContext = ExecutionContext.Implicits.global
+    Future {
+      Thread.sleep(retryDelay)
+    }.onComplete(_ => {
+      cb.attemptResetBrokenState(this, 0)
+    })
 
     override def preInvoke(): Unit = {
       cb.invocationListeners.foreach { listener =>
@@ -312,6 +377,47 @@ private object CircuitBreaker {
           listener.onInvocationInBrokenState,
           cb.name,
           "notify listener of invocation in broken state"
+        )
+      }
+
+      //immediately fail
+      throw new CircuitBreakerBrokenException(
+        cb.name,
+        s"Making ${cb.name} unavailable after ${cb.failLimit} errors"
+      )
+    }
+
+    override def postInvoke(): Unit = {
+
+      /* do nothing */
+    }
+
+    override def onThrowable(e: Throwable): Unit = {
+
+      /* do nothing */
+    }
+
+    override def onFailure(): Unit = {
+
+      /* do nothing */
+    }
+  }
+
+  /**
+    * CircuitBreaker is opened/waiting. Invocations are attempted
+    */
+  class AttemptResetState(cb: CircuitBreaker, retryCount: Int = 0) extends State {
+    val retryAt: Long = retryCount match {
+      case 0 => System.currentTimeMillis()
+      case _ => System.currentTimeMillis() + cb.calcRetryDelay(retryCount)
+    }
+
+    override def preInvoke(): Unit = {
+      cb.invocationListeners.foreach { listener =>
+        safely(cb.notificationsExecutionContext)(
+          listener.onInvocationInAttemptResetState,
+          cb.name,
+          "notify listener of invocation in attempt reset state"
         )
       }
 
@@ -329,30 +435,44 @@ private object CircuitBreaker {
       // Called after a successful retry.
       cb.reset()
 
-    override def onThrowable(e: Throwable): Unit = { /* do nothing */ }
+    override def onThrowable(e: Throwable): Unit = {
 
-    override def onFailure(): Unit = { /* do nothing */ }
+      /* do nothing */
+    }
+
+    override def onFailure(): Unit = {
+
+      /* do nothing */
+    }
   }
+
 }
 
 /**
   * Builder for [[CircuitBreaker]]
   *
-  * @param name the name of the circuit breaker
-  * @param failLimit maximum number of consecutive failures before the circuit breaker is tripped (opened)
-  * @param retryDelay duration until an open/broken circuit breaker lets a call through to verify whether or not it should be reset
-  * @param isResultFailure partial function to allow users to determine return cases which should be considered as failures
+  * @param name                  the name of the circuit breaker
+  * @param failLimit             maximum number of consecutive failures before the circuit breaker is tripped (opened)
+  * @param retryDelay            duration until an open/broken circuit breaker lets a call through to verify whether or not it should be reset
+  * @param isExponentialBackoff  indicating the retry delayed should be increased exponential on consecutive failures
+  * @param exponentialRetryCap   limits the number of times the retryDelay will be increased exponentially, ignored if not exponential backoff
+  * @param isResultFailure       partial function to allow users to determine return cases which should be considered as failures
   * @param isExceptionNotFailure partial function to allow users to determine exceptions which should not be considered failures
-  * @param stateChangeListeners listeners that will be notified when the circuit breaker changes state (open <--> closed)
-  * @param invocationListeners listeners that will be notified whenever the circuit breaker handles a method/function call
+  * @param stateChangeListeners  listeners that will be notified when the circuit breaker changes state (open <--> closed)
+  * @param invocationListeners   listeners that will be notified whenever the circuit breaker handles a method/function call
   */
 case class CircuitBreakerBuilder(
   name: String,
   failLimit: Int,
   retryDelay: FiniteDuration,
   isExponentialBackoff: Boolean = false,
-  isResultFailure: PartialFunction[Any, Boolean] = { case _ => false },
-  isExceptionNotFailure: PartialFunction[Throwable, Boolean] = { case _ => false },
+  exponentialRetryCap: Option[Int] = Some(10),
+  isResultFailure: PartialFunction[Any, Boolean] = {
+    case _ => false
+  },
+  isExceptionNotFailure: PartialFunction[Throwable, Boolean] = {
+    case _ => false
+  },
   stateChangeListeners: List[CircuitBreakerStateChangeListener] = List(),
   invocationListeners: List[CircuitBreakerInvocationListener] = List(),
   notificationsExecutionContext: Option[ExecutionContext] = None
@@ -363,7 +483,7 @@ case class CircuitBreakerBuilder(
     * circuit breaker. This can be useful when the wrapped/underlying call does not use exceptions to communicate failures.
     *
     * @param resultFailureCases partial function used to determine whether or not the returned values should be recorded as a
-    *                     failure by the circuit breaker
+    *                           failure by the circuit breaker
     * @return a builder, for chaining configuration calls
     */
   def withResultFailureCases(resultFailureCases: PartialFunction[Any, Boolean]): CircuitBreakerBuilder =
@@ -390,8 +510,8 @@ case class CircuitBreakerBuilder(
     * the circuit breaker and request processing logic from adverse side effects (e.g. high latency or blocking)
     * that may occur on the listeners' registered functions.
     * A dedicated, single-thread execution context is used by default.
-    * @see withNotificationsExecutionContext
     *
+    * @see withNotificationsExecutionContext
     * @param stateChangeListeners the list of listeners
     * @return a builder, for chaining configuration calls
     */
@@ -407,8 +527,8 @@ case class CircuitBreakerBuilder(
     * the circuit breaker and request processing logic from adverse side effects (e.g. high latency or blocking)
     * that may occur on the listeners' registered functions.
     * A dedicated, single-thread execution context is used by default.
-    * @see withNotificationsExecutionContext
     *
+    * @see withNotificationsExecutionContext
     * @param invocationListeners the list of listeners
     * @return a builder, for chaining configuration calls
     */
